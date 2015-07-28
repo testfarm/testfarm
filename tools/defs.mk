f1681d8ddf181e798bf88777420c5ab9987fc231
Sylvain Giroudon <sylvain.giroudon@testfarm.org>
2015-07-28 10:44:28 +0200

    ENH: Install LICENSE file

--- tools/defs.mk
+++ tools/defs.mk
@@ -1,3 +1,24 @@
+##
+##    TestFarm -- Common Makefile rules and definitions
+##
+##    This file is part of TestFarm,
+##    the Test Automation Tool for Embedded Software.
+##    Please visit http://www.testfarm.org.
+##
+##    TestFarm is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    TestFarm is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 #
 # Commands
 #
@@ -26,6 +47,7 @@ PKGDIR = $(STAGING_DIR)/$(PKGNAME)/$(ARCH)
 DESTDIR = $(PKGDIR)/root
 INSTALLROOT = $(DESTDIR)
 INSTALLDIR = $(DESTDIR)/opt/testfarm
+DOCDIR = $(DESTDIR)/usr/share/doc/testfarm
 
 #
 # Version numbers
